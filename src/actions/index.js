10043d2f04c6a3b5af43d835f92797e3b8e949b5
cristianrojas-dev <cristianrojas.dev@gmail.com>
2018-01-12 02:30:48 -0300

    Add axios to index.js
    -import axios in file index.js
    -integrate to function
    
    	modified:   src/actions/index.js

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,5 @@
+import axios from 'axios'
+
 const API_KEY = '554d852dd34f44c366abc27efe718556';
 const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
 
@@ -5,6 +7,7 @@ export const FETCH_WEATHER = 'FETCH_WEATHER'
 
 export function fetchWeather(city) {
     const url = `${ROOT_URL}&q=${city},us`;
+    const request = axios.get(url);
 
     return {
         type: FETCH_WEATHER
