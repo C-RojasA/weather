f4a47015945fe5b8ea26f07cee61a760745d4dc7
cristianrojas-dev <cristianrojas.dev@gmail.com>
2018-01-12 02:22:34 -0300

    Add data of api
    	modified:   src/actions/index.js

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,12 @@
 const API_KEY = '554d852dd34f44c366abc27efe718556';
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+export const FETCH_WEATHER = 'FETCH_WEATHER'
+
+export function fetchWeather(city) {
+    const url = `${ROOT_URL}&q=${city},us`;
+
+    return {
+        type: FETCH_WEATHER
+    };
+}
